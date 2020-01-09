8236d8a851b5f5495a65d323d70889aaed72e6ac
Sven Gladines <sven.gladines@gmail.com>
2020-01-09 15:23:00 +0100

    upgrade to google cloud

--- src/main/java/be/pirlewiet/digitaal/domain/scenario/DeleteOldEntitiesScenario.java
+++ src/main/java/be/pirlewiet/digitaal/domain/scenario/DeleteOldEntitiesScenario.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.springframework.transaction.annotation.Transactional;
-
-import be.pirlewiet.digitaal.domain.people.Secretary;
-import be.pirlewiet.digitaal.model.Organisation;
+import be.pirlewiet.digitaal.model.Application;
+import be.pirlewiet.digitaal.model.Enrollment;
 import be.pirlewiet.digitaal.model.QuestionAndAnswer;
-import be.pirlewiet.digitaal.repositories.OrganisationRepository;
+import be.pirlewiet.digitaal.repositories.ApplicationRepository;
+import be.pirlewiet.digitaal.repositories.EnrollmentRepository;
 import be.pirlewiet.digitaal.repositories.QuestionAndAnswerRepository;
 import be.pirlewiet.digitaal.web.util.DataGuard;
 
@@ -17,6 +16,12 @@ public class DeleteOldEntitiesScenario extends Scenario {
 	
 	@Resource
 	QuestionAndAnswerRepository questionAndAnswerRepository;
+	
+	@Resource
+	EnrollmentRepository enrollmentRepository;
+	
+	@Resource
+	ApplicationRepository applicationRepository;
 	
 	@Resource
 	DataGuard dataGuard;
@@ -29,13 +34,44 @@ public class DeleteOldEntitiesScenario extends Scenario {
 	@Override
 	//@Transactional
 	public void execute( String... parameters ) {
+
+		try {
+			List<QuestionAndAnswer> qnas
+				= this.questionAndAnswerRepository.findAll();
+			
+			logger.info( "about to delete [{}] qnas", qnas.size() );
+			
+			this.questionAndAnswerRepository.deleteAllInBatch();
+		}
+		catch( Exception e ) {
+			logger.warn( "exception while removing old qnas", e  );
+		}
 		
-		List<QuestionAndAnswer> qnas
-			= this.questionAndAnswerRepository.findAll();
+		try {
+			List<Enrollment> all
+				= this.enrollmentRepository.findAll();
+			
+			logger.info( "about to delete [{}] enrollments", all.size() );
+			
+			this.enrollmentRepository.deleteAllInBatch();
+		}
 		
-		logger.info( "about to delete [{}] qnas", qnas.size() );
+		catch( Exception e ) {
+			logger.warn( "exception while removing old enrollments", e  );
+		}
 		
-		this.questionAndAnswerRepository.deleteAllInBatch();
+		try {
+			List<Application> all
+				= this.applicationRepository.findAll();
+			
+			logger.info( "about to delete [{}] applications", all.size() );
+			
+			this.applicationRepository.deleteAllInBatch();
+		}
+		
+		catch( Exception e ) {
+			logger.warn( "exception while removing old applications", e  );
+		}
 		
 	}
 	
