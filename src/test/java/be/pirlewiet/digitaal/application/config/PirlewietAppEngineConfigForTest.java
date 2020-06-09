20d74f02d417778642fc80326525539aed305bef
Sven Gladines <sven.gladines@gmail.com>
2020-06-09 21:27:36 +0200

    covid-19, cancel holidays

--- src/test/java/be/pirlewiet/digitaal/application/config/PirlewietAppEngineConfigForTest.java
+++ src/test/java/be/pirlewiet/digitaal/application/config/PirlewietAppEngineConfigForTest.java
@@ -1,21 +1,8 @@
 package be.pirlewiet.digitaal.application.config;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.spi.PersistenceProvider;
-
-import org.datanucleus.api.jpa.PersistenceProviderImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import be.occam.utils.spring.configuration.ConfigurationProfiles;
-import be.pirlewiet.digitaal.web.util.DataGuard;
-import be.pirlewiet.digitaal.web.util.DevGuard;
 
 import com.google.appengine.tools.development.testing.LocalAppIdentityServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
@@ -23,60 +10,15 @@ import com.google.appengine.tools.development.testing.LocalMailServiceTestConfig
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.apphosting.api.ApiProxy;
 
+import be.occam.utils.spring.configuration.ConfigurationProfiles;
+import be.pirlewiet.digitaal.web.util.DataGuard;
+import be.pirlewiet.digitaal.web.util.DevGuard;
+
 @Configuration
 @Profile({ConfigurationProfiles.DEV})
 public class PirlewietAppEngineConfigForTest {
 	
 	final static String JPA_PKG = "be.pirlewiet.digitaal";
-	
-	@Configuration
-	@EnableJpaRepositories(JPA_PKG)
-	@Profile( { ConfigurationProfiles.DEV, ConfigurationProfiles.TEST } )
-	static class EntityManagerConfig {
-		
-		@Bean
-		public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean(PersistenceProvider persistenceProvider, LocalServiceTestHelper dataStoreHelper ) {
-			
-			LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-			factory.setPackagesToScan( "be.pirlewiet.digitaal.model" );
-			factory.setPersistenceProvider( persistenceProvider );
-			// factory.setDataSource(jpaDataSource);
-			factory.setPersistenceUnitName("pirlewiet-digitaal-dev-test");
-			factory.getJpaPropertyMap().put( "datanucleus.jpa.addClassTransformer", "false" );
-			factory.getJpaPropertyMap().put( "datanucleus.appengine.datastoreEnableXGTransactions", "true" );
-			factory.getJpaPropertyMap().put( "datanucleus.metadata.allowXML", "false" );
-			factory.afterPropertiesSet();
-			return factory;
-		}
-		
-		@Bean
-		PersistenceProvider persistenceProvider() {
-			
-			PersistenceProviderImpl provider
-				= new PersistenceProviderImpl();
-			
-			return provider;
-			
-		}
-
-		@Bean
-		public EntityManagerFactory entityManagerFactory(LocalContainerEntityManagerFactoryBean factory) {
-			return factory.getObject();
-		}
-
-		@Bean
-		public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-			return new PersistenceExceptionTranslationPostProcessor();
-		}
-
-		@Bean
-		public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-			JpaTransactionManager transactionManager = new JpaTransactionManager();
-			transactionManager.setEntityManagerFactory(entityManagerFactory);
-			return transactionManager;
-		}
-		
-	}
 	
 	@Configuration
 	@Profile( { ConfigurationProfiles.DEV, ConfigurationProfiles.TEST } )
